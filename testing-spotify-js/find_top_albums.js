6eda71f47d810ed98e179893fd804cca283e48e4
emmett1999 <83946630+emmett1999@users.noreply.github.com>
2025-06-29 14:37:30 -0500

    Update find_top_albums.js

--- testing-spotify-js/find_top_albums.js
+++ testing-spotify-js/find_top_albums.js
@@ -1,14 +1,23 @@
 // {
-//   access_token: 'BQDrCulvuNKZM3InYLv2n2AYmuqNPDl7d4PVsk4OHmA7074y2CcbAAZdjbJbYqo4IrNCxr3s4T3wvnbraaCqgqpDXR_jgx18LbLRWvH9_0L6fWzy0iVpUr6DqrcFTs2jXJPuZoxuBp_OFpYTrnHqrp--Y_zrMphk_CziygD17Be7jzFqwQ8VvOEOahFMdapMthpTaUG4lb1ZbD44Njbz',
+//   access_token: 'BQDgBQANFgXXZT1wASqHy14M4XRHSaMa0YaJofP4w69pc7OMwWmvc6G6f078Du97hvmNqE9EzQzqC0ZAPcgownN7cSizFcVtCgfd5fAsQLqG1A3VdwCAiXCtGrjMt3JuXcrFVg8KWl-ZN1RFv3by4QpxnjXg57jvbxxrwME-Bvnu_cYLuebc2ploNsnb6Rl5MxMHSL1Q1qtINO6fCCY1QiJmBQI_vBtzANmG8AfIZGgOIlHUMvfk',
 //   token_type: 'Bearer',
 //   expires_in: 3600,
-//   refresh_token: 'AQDW60VNmJVaiORF1VhTMgSuRtnQgbalx9XSo54YSNjDz9bu5LPaoz4tP7_N1KgIS2WAUACxjIQYn3bgOqVMuHnl5uVkunO1qKzEm8qMEdGA2PDmdgC8DeJ0zHmGrOpma-w',
+//   refresh_token: 'AQClv7WyTqSWNAkrnZatSs4W_47ostU2qMlkbHUY9ek-XIpNT3N-3Nt8TsYxFpQeHN0GkQOr6Yh53Tnx3feeN8IRxLNieVVkoTDbURiY_7c2VbtM4W469rveQFCPmnJ1CPI',
 //   scope: 'user-library-read user-read-email user-read-private'
 // }
 
-const token = 'BQBLWMpEC1xQsW8AWyQFs11AziTMqPDXhOLNechaLjkYtviC8cithzWGoptNNerRmCktovgMNNIjj5MWnV4nzbxnRFyKoub3PlAFeQa5uU9J4Yi9jYFWrvjRCfGsNogTDEPFm2aJUQcxcJczknHUJ9eGIeRog2j_lvSlrs1T6ZReig3MH7YxGDCff4DoewQ4JMDZu8xSG7LjRgw90qnB';
-async function fetchWebApi(endpoint, method, body) {
-  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+// 2025-06-24 NEW token
+// {
+//   access_token: 'BQDZ_NAvyqNep31YlE0kaD1Bop1YssckNFUoNOpJ1yFu2kqqy4XZvEVyJr2MnGDaPZnJXnPr5emdeOlRuhJxqnF6MXt7AeJpII-_aw_BqjfagfSoptTMl-cLgz1VUqlCiU9PC5dBW1AByxpKhVsp-6MByUc5xrRGiObPxLeBqee2eSFxU2FDaXN4i0wUcNRJkOWK9x8p-mH0TFD314CfsRGcdvksvLgkZz2ReO-crgK8eKJ9_h19',
+//   token_type: 'Bearer',
+//   expires_in: 3600,
+//   refresh_token: 'AQDbADRuqw-vLWz17xyfh5zFm3tvvZj6TuT8kIn0Nw_7m3b_2klZIZrWpmmtHu_OUi_HIXOA67NYvWiRltXLPQNtDOc84A3o3faBJNtbtmK5fVAW8NpAlGYZpcsByTvv6m8',
+//   scope: 'user-library-read user-read-email user-read-private'
+// }
+
+const token = 'BQA7Oe1n1KNK6xP6KXSWo8Uk8eN68P5pXCbjGZyoe9lS1yfJ5rCGsvGcfj3mWgBmB4crFQKTu_FMwnsNkbj2aEe-Ew5cc9-h0vu2drJTUIAF5vxcR7OOTWNQBci1D2aIHAPrKOJmdpkfMaz5BIe4Zbg8wiuO8jsCnTgfU34wYRho8vGZqEPHwdI1tHeDg5AbbqL6HU6DnZlZzd3UpOkJOT4QI2BSkQV9HS-xDuJ_tNlXBXxfURdG';
+async function fetchWebApi(url, method, body) {
+  const res = await fetch(url, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
@@ -18,33 +27,69 @@ async function fetchWebApi(endpoint, method, body) {
   return await res.json();
 }
 
-async function getAllAlbums(){
-    return (await fetchWebApi(
-      'v1/me/albums?offset=0&limit=50', 'GET'
-    ));
-  }
+async function getAllAlbums(url){
+  return (await fetchWebApi(url, 'GET'));
+}
   
 async function run() {
-  const albumsResponse = await getAllAlbums();
-  var nextUrl = albumsResponse.next;
-  console.log("Next URL: " + nextUrl);
-  var albums = albumsResponse.items;
-  const albumInfoList = albums?.map((albumResult) => {
-    const albumInfo = {};
-    albumInfo['albumName'] = albumResult.album.name;
+  const initialUrl = 'https://api.spotify.com/v1/me/albums?offset=0&limit=50';
 
-    const albumArtists = albumResult.album.artists;
-    const artistList = [];
-    albumArtists.forEach((artist) => artistList.push(artist.name));
-    albumInfo['albumArtists'] = artistList;
-    return albumInfo;
-  });
-  console.log(albumInfoList);
+  var albumsResponse = await getAllAlbums(initialUrl);
+  if(!albumsResponse) {
+    // console.error(new Error("No initial album response"));
+    throw "No albums response";
+  }
+  
+  var masterAlbumInfoList = [];
+
+  while(true) {
+
+    var albums = albumsResponse.items;
+    const albumInfoList = albums?.map((albumResult) => {
+      const albumInfo = {};
+      albumInfo['albumName'] = albumResult.album.name;
+  
+      const albumArtists = albumResult.album.artists;
+      const artistList = [];
+      albumArtists.forEach((artist) => artistList.push(artist.name));
+      albumInfo['albumArtists'] = artistList;
+      return albumInfo;
+    });
+    masterAlbumInfoList.push(...albumInfoList);
+
+    nextUrl = albumsResponse.next;
+    if(!nextUrl) {
+      break;
+    }
+    console.log("Next URL: " + nextUrl);
+    albumsResponse = await getAllAlbums(nextUrl);
+    // console.log("Albums response: " + albumsResponse.album);
+  }
+
+  console.log('Completed pulling albums from Spotify');
+  console.log('Master album list size: %s', masterAlbumInfoList.length);
+
+  // TODO: make a separate function for this
+  // masterAlbumInfoList.sort((a, b) => {
+  //   const nameA = a.albumArtists[0]?.name.toLowerCase() || '';
+  //   const nameB = b.albumArtists[0]?.name.toLowerCase() || '';
+  //   return nameA.localeCompare(nameB);
+  // });
+
+  // masterAlbumInfoList.forEach(album => {
+  //   const artistNames = album.albumArtists.map(artist => artist.name).join(', ');
+  //   console.log(`${artistNames}, ${album.albumName}`);
+  // });
+
+  console.log(masterAlbumInfoList);
   // + albumResult.album.artists.forEach((artist) => artist.name)
   // '' + albumResult.album.name + ', ' + albumResult.album.artists[0].name
 
   // console.log(albums);
 }
+
+// CODE EXECUTION
+run();
 
 
 // async function getTopTracks(){
@@ -65,4 +110,3 @@ async function run() {
 //     );
 // }
 
-run();
